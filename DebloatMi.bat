c6dc476e88cdc87e2eed992ae63e0d9679cde4cc
mage1k99 <46259860+mage1k99@users.noreply.github.com>
2019-09-02 09:29:44 +0530

    Updated list
    Removed Some Google Apps , Added More Mi Apps that Are  Safe to remove

--- DebloatMi.bat
+++ DebloatMi.bat
@@ -6,9 +6,6 @@ adb shell pm uninstall -k --user 0 com.android.mms rem Mensajes
 adb shell pm uninstall -k --user 0 com.facebook.appmanager rem Facebook App Manager
 adb shell pm uninstall -k --user 0 com.facebook.services rem Facebook Services
 adb shell pm uninstall -k --user 0 com.facebook.system rem Facebook App Installer
-adb shell pm uninstall -k --user 0 com.google.android.apps.docs rem Google Drive
-adb shell pm uninstall -k --user 0 com.google.android.apps.maps rem Google Maps
-adb shell pm uninstall -k --user 0 com.google.android.apps.photos rem Google Photos
 adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon rem Google Duo
 adb shell pm uninstall -k --user 0 com.google.android.music rem Google Music
 adb shell pm uninstall -k --user 0 com.google.android.videos rem Play Movies
@@ -26,3 +23,14 @@ adb shell pm uninstall -k --user 0 com.xiaomi.glgm rem Mi games
 adb shell pm uninstall -k --user 0 com.miui.hybrid rem Quick Apps
 adb shell pm uninstall -k --user 0 com.xiaomi.payment rem MiPay
 adb shell pm uninstall -k --user 0 com.mipay.wallet.id rem Mipay Id 
+adb shell pm uninstall -k --user 0 com.xiaomi.o2o rem o2o
+adb shell pm uninstall -k --user 0 com.xiaomi.vipaccount rem VIP account , found in some devices
+adb shell pm uninstall -k --user 0 com.xiaomi.joyose rem joyose
+adb shell pm uninstall -k --user 0 com.xiaomi.lens rem Xiaomi lens
+adb shell pm uninstall -k --user 0 com.xiaomi.pass rem xioami Pass
+adb shell pm uninstall -k --user 0 com.miui.systemAdSolution rem SystemAdsolution
+adb shell pm uninstall -k --user 0 com.baidu.input_mi rem Mi input
+adb shell pm uninstall -k --user 0 com.baidu.duersdk.opensdk rem baidu open sdk
+adb shell pm uninstall -k --user 0 com.autonavi.minimap rem autonavi minimap
+adb shell pm uninstall -k --user 0 com.miui.daemon rem miui daemon
+adb shell pm uninstall -k --user 0 com.miui.contentcatcher rem content catcher
